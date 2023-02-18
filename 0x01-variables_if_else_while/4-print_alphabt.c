170edc7061553f9c752445b712a24a2c70b70910
Ntombeigh <Ntombeighzitha@gmail.com>
2023-02-17 18:53:28 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
+ * except q and e
+ * Return: Aways 0 
  */
 int main(void)
 {
