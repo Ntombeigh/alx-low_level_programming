004c6347dc937921694ec74339f8b88371742e98
Ntombeigh <Ntombeighzitha@gmail.com>
2023-02-21 04:17:09 -0800

    0-preprocessor

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio .h>
+
+/* program that prints _putchar, followed by a new line.
+
+* The program should return 0
+*/
+
+int main(void)
+{ 
+	char name = _putchar
+
+		printf('%c', name)
+
+ } 
