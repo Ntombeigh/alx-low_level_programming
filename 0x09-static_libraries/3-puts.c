5f1816c7e2b24a4e042ec0fe5d8b5c0ccf7d73c1
Ntombeigh <Ntombeighzitha@gmail.com>
2023-12-10 23:00:17 -0800

    create a library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void _puts(char *str);
+#include <stdio.h>
+/**
+ * main - Entry point
+ * _puts - prints the string
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
